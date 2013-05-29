68ad80fe8f9ae8167bcd8ea917beed976d9590dd
Joseph the Magnificent <thedodo1234@hotmail.com>
2013-05-29 20:36:15 +1000

    per hour
    trying to fix so exp is good

--- UTIL.java
+++ UTIL.java
@@ -2,6 +2,6 @@ package mining;
 
 public class UTIL {
 	public static int getPerHour(final int value){
-		return (int)(value * 3600000 / VARS.timeRunning);
+		return (int)(value * (3600000 / VARS.timeRunning));
 	}
 }
